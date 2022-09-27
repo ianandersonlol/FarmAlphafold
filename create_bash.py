3362e3e035ffac451c1dfbc81e6bc2208cc3380b
Ian Anderson <ian033190@gmail.com>
2022-09-27 13:55:21 -0700

    Fixed singularity issue and formatting.
    I really hate myself that I always decide that aesthetics are important. In reality there should be no prints no nothing, just do your job. But noooooo I want the user to know something is going on.

--- create_bash.py
+++ create_bash.py
@@ -1,40 +1,46 @@
 import sys
 import os
 import time
-
+import colorama
+from colorama import Fore
 # Look for output/input directories.
 
 if len(sys.argv) == 1:
-    print("What's going on here buddy?\nYou need to give me a name...\nplease")
+    print(Fore.RED + "What's going on here buddy?\nYou need to give me a name...\nplease")
+    time.sleep(.5)    
     exit() 
 
 if len(sys.argv) ==2:
-    print("Ok boys, girls, and NBs\n We need to do both a an output director AND an input directory")
+    print(Fore.RED + "Ok boys, girls, and NBs\n We need to do both a an output director AND an input directory")
+    time.sleep(.5)
     exit()
 if len(sys.argv) == 3:
-    print('You need to also give your e-mail address for logs')
+    print(Fore.RED + 'You need to also give your e-mail address for logs')
+    time.sleep(.5)
     exit()
 if len(sys.argv) == 4:
-    print('You must aslo give me your farm name so we can put everything into your slurm_logs')
-
+    print(Fore.RED + 'You must aslo give me your farm name so we can put everything into your slurm_logs\n Exiting...')
+    time.sleep(.5)
+    exit()
 if len(sys.argv) >5 :
-    print("Woah there cowbow!\nThis is UNIX!\nLet's avoid spaces in directory names, shall we?\nDon't try to be smart by putting the whole thing in quotes either!")
+    print(Fore.RED + "Woah there cowbow!\nThis is UNIX!\nLet's avoid spaces in directory names, shall we?\nDon't try to be smart by putting the whole thing in quotes either!")
+    time.sleep(.5)
     exit()
 
-print('Validating paths...')
+print(Fore.GREEN+'Validating paths...')
 time.sleep(.5) # I hate myself for doing this. I just like it looking cool.
 
 if sys.argv[1][0] != '/' or sys.argv[1][-1] != '/':
-    print('ERROR: Just a reminder. The farm needs your directories to have a slash "/" before and after the path. Make sure you included that or it won\'t work ')
+    print(Fore.RED + 'ERROR: Just a reminder. The farm needs your directories to have a slash "/" before and after the path. Make sure you included that or it won\'t work ')
+    time.sleep(.5)
     exit() 
 
 if sys.argv[2][0] != '/' or sys.argv[2][-1] != '/':
-    print('ERROR: Just a reminder. The farm needs your directories to have a slash "/" before and after the path. Make sure you included that or it won\'t work ')
+    print(Fore.RED + 'ERROR: Just a reminder. The farm needs your directories to have a slash "/" before and after the path. Make sure you included that or it won\'t work ')
+    time.sleep(.5)
     exit()
-print('Validation complete. You have a real path.')
-time.sleep(.5) #Again, I hate myself. Why am I like this?  
-print('Generating Shell Files.')
-time.sleep(1) # this sleep is to make it more pleasent on the eyes. WHHHHHHHHHYYYYY?!?!?!?! 
+print(Fore.GREEN + 'Validation complete. You have a real path.')
+time.sleep(1) #Again, I hate myself. Why am I like this?  
 
 file_name = str(int(time.process_time()*1000000))
 
@@ -43,8 +49,9 @@ current_path = os.path.abspath(os.getcwd())
 maindir = sys.argv[1]
 
 if ' ' in maindir:
-    print("Okay, look smart guy/gal/non-binary pal.\nThis is UNIX.\nYou REALLY shouldn't be putting spaces in your directory names!!!\nBE BETTER!")
-
+    print(Fore.RED + "Okay, look smart guy/gal/non-binary pal.\nThis is UNIX.\nYou REALLY shouldn't be putting spaces in your directory names!!!\nBE BETTER!")
+    time.sleep(.5)
+    exit()
 newpath = os.path.join(current_path, maindir) 
 
 
@@ -62,18 +69,16 @@ def write_initialization():
     print('#SBATCH --mail-user='+sys.argv[3],file = o)
     print('set -e',file = o)
     print('set -u',file = o)
-    print('module load spack/singularity/3.8.0',file = o)
+    print('module load spack/singularity/3.8.3',file = o)
     print('singularity instance start -B /home/haryu/alphafoldDownload /home/icanders/alphafold.sif bash',file = o)
     print('singularity exec instance://bash ~/'+file_name+'.sh',file = o)
 
 
 def write_loop(output_dir,input_dir):
     
-    print('Generating Files With Name: '+file_name)
+    print(Fore.GREEN + 'Generating Files With Name: '+file_name)
     o = open(file_name+'.sh', 'w')
-    #print(file_name+'.sh',file=o) I don't think we actually need this. Do we? 
     print('#! /bin/bash\n\n',file=o)
-    #print('singularity exec --nv -B /home/haryu/alphafoldDownload alphafold.sif bash',file = o) oops I actually don't need this
     print('source /opt/miniconda3/etc/profile.d/conda.sh', file = o)
     print('conda activate alphafold', file = o)
     print('cd /opt/alphafold/', file = o)
@@ -87,3 +92,6 @@ def write_loop(output_dir,input_dir):
 
 write_loop(sys.argv[1],sys.argv[2])
 write_initialization()
+
+time.sleep(1) #IAN STOP YOU MONSTER AESTHETICS ARE FOR THE WEAK
+print(Fore.YELLOW + 'Complete!') #WHY ARE WE LOADING A MODULE JUST FOR A COLOR! IAN STOP.
