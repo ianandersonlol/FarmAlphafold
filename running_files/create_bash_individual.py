e3c33b3b196291aa81b444e716be09e992a66a68
Ian Anderson <ian033190@gmail.com>
2022-10-23 10:50:10 -0700

    committing changes to work on gpu cluster
    lol

--- running_files/create_bash_individual.py
+++ running_files/create_bash_individual.py
@@ -7,23 +7,33 @@ from colorama import Fore
 
 if len(sys.argv) == 1:
     print(Fore.RED + "What's going on here buddy?\nYou need to give me a name...\nplease")
-    time.sleep(.5)    
+    time.sleep(.5)
+    print(Fore.RED + "<output_dir>,<input_path>,<email>,<davis_id>")
+    time.sleep(.5)        
     exit() 
 if len(sys.argv) ==2:
     print(Fore.RED + "Ok boys, girls, and NBs\n We need to do both a an output director AND an input directory")
     time.sleep(.5)
+    print(Fore.RED + "<output_dir>,<input_path>,<email>,<davis_id>")
+    time.sleep(.5)        
     exit()
 if len(sys.argv) == 3:
     print(Fore.RED + 'You need to also give your e-mail address for logs')
     time.sleep(.5)
+    print(Fore.RED + "<output_dir>,<input_path>,<email>,<davis_id>")
+    time.sleep(.5)        
     exit()
 if len(sys.argv) == 4:
     print(Fore.RED + 'You must aslo give me your farm name so we can put everything into your slurm_logs\n Exiting...')
     time.sleep(.5)
+    print(Fore.RED + "<output_dir>,<input_path>,<email>,<davis_id>")
+    time.sleep(.5)        
     exit()
 if len(sys.argv) >5 :
     print(Fore.RED + "Woah there cowbow!\nThis is UNIX!\nLet's avoid spaces in directory names, shall we?\nDon't try to be smart by putting the whole thing in quotes either!")
     time.sleep(.5)
+    print(Fore.RED + "<output_dir>,<input_path>,<email>,<davis_id>")
+    time.sleep(.5)        
     exit()
 
 print(os.path.basename(sys.argv[2]))
@@ -34,18 +44,20 @@ time.sleep(.5) # I hate myself for doing this. I just like it looking cool.
 if sys.argv[1][0] != '/' or sys.argv[1][-1] != '/':
     print(Fore.RED + 'ERROR: Just a reminder. The farm needs your directories to have a slash "/" before and after the path. Make sure you included that or it won\'t work ')
     time.sleep(.5)
+    print(Fore.RED + "<output_dir>,<input_path>,<email>,<davis_id>")
+    time.sleep(.5)        
     exit() 
 
 if sys.argv[2][0] != '/':
     print(Fore.RED + 'ERROR: Just a reminder. The farm needs your directories to have a slash "/" before. Make sure you included that or it won\'t work ')
     time.sleep(.5)
+    print(Fore.RED + "<output_dir>,<input_path>,<email>,<davis_id>")
+    time.sleep(.5)        
     exit()
 
 if not sys.argv[2].endswith('.fasta') and not sys.argv[2].endswith('.fa'):
     print(Fore.RED,'Your output file needs to be either .fasta or .fa')
 
-print(Fore.GREEN + 'Validation complete. You have a real path.')
-time.sleep(1) #Again, I hate myself. Why am I like this?  
 
 file_name = str(int(time.process_time()*1000000))
 
@@ -56,8 +68,13 @@ maindir = sys.argv[1]
 if ' ' in maindir:
     print(Fore.RED + "Okay, look smart guy/gal/non-binary pal.\nThis is UNIX.\nYou REALLY shouldn't be putting spaces in your directory names!!!\nBE BETTER!")
     time.sleep(.5)
+    print(Fore.RED + "<output_dir>,<input_path>,<email>,<davis_id>")
+    time.sleep(.5)        
     exit()
 newpath = os.path.join(current_path, maindir) 
+
+print(Fore.GREEN + 'Validation complete. You have a real path.')
+time.sleep(1) #Again, I hate myself. Why am I like this?  
 
 
 def write_initialization():
@@ -67,9 +84,9 @@ def write_initialization():
     print('#SBATCH -e /home/'+sys.argv[4]+'/slurm-log/'+file_name+'_errors.txt',file = o)
     print('#SBATCH -J '+file_name+'_job',file = o)
     print('#SBATCH -t 48:00:00',file = o)
-    print('#SBATCH -c 32',file = o)
-    print('#SBATCH --mem 256G',file = o)
-    print('#SBATCH --partition=bmh',file = o)
+    print('#SBATCH -c 16',file = o)
+    print('#SBATCH --mem 128G',file = o)
+    print('#SBATCH --partition=gpu-a100-h',file = o)
     print('#SBATCH --mail-type=ALL',file = o)
     print('#SBATCH --mail-user='+sys.argv[3],file = o)
     print('set -e',file = o)
--- running_files/doubletest.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-source /opt/miniconda3/etc/profile.d/conda.sh
-conda activate alphafold
-cd /opt/alphafold/
-./run.sh -d /home/haryu/alphafoldDownload -o /home/icanders/alphafold_output -m model_1 -f /home/haryu/alphafold/proteins/Q0JH50.fasta -t 2022-11-03
